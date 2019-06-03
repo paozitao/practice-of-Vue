f0e2330032423b9eba0d0c10d15b42aa66ee8dc9
paozitao <lt5132908@gmail.com>
2019-06-04 01:31:54 +0800

    Initial commit

---
+++ VuePractice/201881/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  <h1>{{ title }}</h1>  
+  <ul>
+    <li v-for="(todo,index) in todos" :id="index">
+      <label>{{ index + 1 }}.{{ todo.value }}</label>
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'app',
+    data () {
+      return {
+        title : "Vue-todos",
+        todos : [
+          { value: "read a book", done: false },
+          { value: "exercise", done: true },
+          { value: "write a article", done: true }
+        ]
+      }
+    }
+  }
+</script>
+
+<style></style>
